eb39553ab3a5f0d8990b5bcaacd4727f39584634
Kyle Brittan <kabrittan@gmail.com>
2018-06-30 11:53:24 -0700

    Updated File Structure

--- simple-todos/client/main.js
+++ simple-todos/client/main.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
- 
+import './main.html';
+
+import '../imports/startup/accounts-config.js';
 import App from '../imports/ui/App.js';
  
 Meteor.startup(() => {
